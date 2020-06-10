0299941349bc38598cff9fff18667fa56e68c94f
gachez <briangach98@gmail.com>
2020-06-10 19:08:56 +0300

    first commit

---
+++ functions/index.js
@@ -0,0 +1,14 @@
+const functions = require('firebase-functions');
+
+// http request 1
+exports.randomNumber = functions.https.onRequest((req,res,) => {
+    const number = Math.round(Math.random()* 100);
+
+    res.send(number.toString());
+});
+
+// http request 2
+exports.toTheDojo = functions.https.onRequest((req,res,) => {
+
+    res.redirect('https://www.tengezastudios.co.ke');
+});
