9c6c6bca3a244dfe32ca9435d7c4593c3452a8bc
gachez <briangach98@gmail.com>
2020-07-13 18:15:03 +0300

    added cors

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,12 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 const mongoose = require('mongoose');
+const cors = require('cors')({origin: true});
 const VirtualLearning = require('./virtualLearningParents.model');
 admin.initializeApp();
+
+  // Configuring the database
+  mongoose.Promise = global.Promise;
 
 // Connecting to the database
 mongoose.connect('mongodb+srv://dbUser:dbPassword@cluster0-cu3wb.gcp.mongodb.net/test?retryWrites=true&w=majority', {
@@ -16,13 +20,11 @@ mongoose.connect('mongodb+srv://dbUser:dbPassword@cluster0-cu3wb.gcp.mongodb.net
       process.exit();
   });
 
-exports.registerLearner = functions.https.onRequest((req, res) => {
-  // Configuring the database
-mongoose.Promise = global.Promise;
-
-
-  let learner = new VirtualLearning(req.body);
-    learner.save()
-    console.log('Success');
-    res.send('Succesfully registered virtual learner' + res);
-  });
+exports.registerLearner = functions.https.onRequest((request, response) => {
+cors(request, response, () => {
+  let learner = new VirtualLearning(request.body);
+  learner.save()
+  console.log('Success');
+  response.status(200).send({success: 'Added virtual learner'});
+  })
+});
