92a56ccbff77c6f258ba9bf4de3b6ec52154ce1b
gachez <briangach98@gmail.com>
2020-07-13 17:28:31 +0300

    added function for virtual learner registration

--- functions/index.js
+++ functions/index.js
@@ -1,10 +1,27 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
+const mongoose = require('mongoose');
 const VirtualLearning = require('./virtualLearningParents.model');
 admin.initializeApp();
 
+// Connecting to the database
+mongoose.connect('mongodb+srv://dbUser:dbPassword@cluster0-cu3wb.gcp.mongodb.net/test?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+  }).then(() => {
+    
+    return true;    
+  }).catch(err => {
+      console.log('Could not connect to the database. Exiting now...', err);
+      process.exit();
+  });
+
 exports.registerLearner = functions.https.onRequest((req, res) => {
-    let learner = new VirtualLearning(req.body);
+  // Configuring the database
+mongoose.Promise = global.Promise;
+
+
+  let learner = new VirtualLearning(req.body);
     learner.save()
     console.log('Success');
     res.send('Succesfully registered virtual learner' + res);
